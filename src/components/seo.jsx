ac16c98c25b8130a10c2ac4c089892b2a7b1b923
Santhoshkumar <117272529+Santhosh-testsigma@users.noreply.github.com>
2023-10-20 12:33:34 +0530

    fixed build error

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -39,31 +39,35 @@ function SEO({ lang,
 
     const isIndexed = !noindex ? 'index,follow' : 'noindex,nofollow'
 
-    const loadGTM = window.location.href.includes("testsigma.com");
+    let loadGTM = false;
+
+    if (typeof window !== 'undefined') {
+        loadGTM = window.location.href.includes("testsigma.com") || window.location.href.includes("https://www.testsigma.com/") || window.location.href.includes("https://testsigma.com/");
+    }
 
     useEffect(() => {
-        if (loadGTM) {
-            const script = document.createElement('script');
-            const noscript = document.createElement('noscript');
-            script.type = 'text/javascript';
-            script.innerHTML = `
-            (function(w,d,s,l,i){
-                w[l]=w[l]||[];
-                w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-                var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-                j.async=true;
-                j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-                f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5F8HTVT');
-            `;
-            noscript.innerHTML = `
-                <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F8HTVT"
-                height="0" width="0" style="display:none;visibility:hidden">
-                </iframe>
-            `
-            document.head.appendChild(script);
-            document.head.appendChild(noscript);
-        }
+    if (loadGTM) {
+        const script = document.createElement('script');
+        const noscript = document.createElement('noscript');
+        script.type = 'text/javascript';
+        script.innerHTML = `
+        (function(w,d,s,l,i){
+            w[l]=w[l]||[];
+            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+            var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+            j.async=true;
+            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+            f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-5F8HTVT');
+        `;
+        noscript.innerHTML = `
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5F8HTVT"
+        height="0" width="0" style="display:none;visibility:hidden">
+        </iframe>
+        `
+        document.head.appendChild(script);
+        document.head.appendChild(noscript);
+    }
     }, []);
 
 
