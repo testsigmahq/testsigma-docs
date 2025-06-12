c6e7ea1a2e65e939be8666f885b31011bdd62057
Santhoshkumar <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-06-12 18:18:42 +0530

    [WEB-1691] - Fixed FC Chat box UI issue

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -180,7 +180,7 @@ function SEO({
         <meta
           httpEquiv='Content-Security-Policy'
           content="default-src 'self' https://*.testsigma.com https://stream.mux.com https://stream.hubilo.com https://s3.us-east-1.amazonaws.com;
-    style-src 'self' https://*.testsigma.com https://fonts.googleapis.com 'unsafe-inline';
+    style-src 'self' https://*.testsigma.com https://fonts.googleapis.com 'unsafe-inline' https://*.freshchat.com;
     font-src 'self' https://fonts.gstatic.com https://*.testsigma.com data: blob:;
     img-src * https: data: blob: about:;
     script-src * https: 'unsafe-inline' 'unsafe-eval';
