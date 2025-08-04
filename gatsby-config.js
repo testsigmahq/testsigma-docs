4e87c9deff51ba3993636dbd4f852b77a8ee93e6
Santhosh-testsigma <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-08-04 17:24:07 +0530

    pathname

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'src',
-        path: `${__dirname}/src/`,
+        path: `${__dirname}/src/docs/`,
       },
     },
     {
