c31645ff931cac8d1ba8fc3feb769032b782f1ef
Santhosh-testsigma <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-08-04 14:22:17 +0530

    Gatsby Config change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,46 +1,53 @@
-require("dotenv").config({
+require('dotenv').config({
   path: `.env`,
 });
 
 module.exports = {
   assetPrefix: process.env.ASSET_HOST,
   siteMetadata: {
-    title: "Testsigma Documentation",
-    description: "",
-    author: "Testsigma",
-    siteUrl: "https://testsigma.com/docs/",
+    title: 'Testsigma Documentation',
+    description: '',
+    author: 'Testsigma',
+    siteUrl: 'https://testsigma.com/docs/',
   },
   flags: {
     DEV_SSR: true,
     FAST_DEV: true,
   },
   plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-image",
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-sass',
+    'gatsby-plugin-image',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      },
+    },
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          "gatsby-plugin-react-helmet",
+          'gatsby-plugin-react-helmet',
           {
-            resolve: "gatsby-remark-embed-video",
+            resolve: 'gatsby-remark-embed-video',
             options: {
               width: 800,
               ratio: 1.77,
               height: 400,
               related: false,
               noIframeBorder: true,
-              loadingStrategy: "lazy",
-              containerClass: "embedVideo-container",
+              loadingStrategy: 'lazy',
+              containerClass: 'embedVideo-container',
               iframeId: false,
             },
           },
-          "gatsby-remark-responsive-iframe",
+          'gatsby-remark-responsive-iframe',
           {
-            resolve: "gatsby-remark-prismjs",
+            resolve: 'gatsby-remark-prismjs',
             options: {
-              classPrefix: "language-",
+              classPrefix: 'language-',
               inlineCodeMarker: null,
               aliases: {},
               showLineNumbers: false,
@@ -59,18 +66,18 @@ module.exports = {
               elements: [`h2`],
             },
           },
-          "gatsby-remark-check-links",
+          'gatsby-remark-check-links',
           {
-            resolve: "gatsby-remark-custom-blocks",
+            resolve: 'gatsby-remark-custom-blocks',
             options: {
               blocks: {
                 danger: {
-                  classes: "alert alert-danger",
-                  title: "optional",
+                  classes: 'alert alert-danger',
+                  title: 'optional',
                 },
                 info: {
-                  classes: "alert alert-info",
-                  title: "optional",
+                  classes: 'alert alert-info',
+                  title: 'optional',
                 },
               },
             },
@@ -78,30 +85,23 @@ module.exports = {
         ],
       },
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
     {
       resolve: `gatsby-plugin-s3`,
       options: {
         bucketName: process.env.BUCKET_NAME,
-        protocol: "https",
+        protocol: 'https',
         hostname: process.env.HOST_NAME,
         generateRedirectObjectsForPermanentRedirects: true,
-      },
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "src",
-        path: `${__dirname}/src/`,
       },
     },
     {
       resolve: `gatsby-plugin-ts-freshchat`,
       options: {
         token: process.env.FRESHCHAT_TOKEN,
-        host: "https://wchat.freshchat.com",
-        appEmbedUrl: "https://app.testsigma.com/ui/dashboard",
+        host: 'https://wchat.freshchat.com',
+        appEmbedUrl: 'https://app.testsigma.com/ui/dashboard',
       },
     },
     {
@@ -128,15 +128,15 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-plugin-env-variables",
+      resolve: 'gatsby-plugin-env-variables',
       options: {
         allowList: [
-          "TYPESENSE_HOST",
-          "TYPESENSE_PORT",
-          "TYPESENSE_PROTOCOL",
-          "TYPESENSE_API_KEY",
-          "TYPESENSE_SEARCH_API_KEY",
-          "TYPESENSE_COLLECTION",
+          'TYPESENSE_HOST',
+          'TYPESENSE_PORT',
+          'TYPESENSE_PROTOCOL',
+          'TYPESENSE_API_KEY',
+          'TYPESENSE_SEARCH_API_KEY',
+          'TYPESENSE_COLLECTION',
         ],
       },
     },
