5b20ab018cd3fcbdc34142705e94a161a5aeac29
Santhosh-testsigma <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-08-05 15:28:35 +0530

    trail and error #7

--- gatsby-config.js
+++ gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
