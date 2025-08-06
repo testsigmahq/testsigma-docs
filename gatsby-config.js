8d30fbbf9e75c11e3c18091f45f21e90fa794762
Santhosh-testsigma <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-08-06 17:46:57 +0530

    trail and error 3

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,16 @@ module.exports = {
               },
             },
           },
+          {
+            resolve: 'gatsby-remark-prismjs',
+            options: {
+              classPrefix: 'language-',
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: false,
+              noInlineHighlight: false,
+            },
+          },
         ],
       },
     },
