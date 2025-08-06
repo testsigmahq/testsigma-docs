4299b808c77f3a6cf02bce8fea998b6e272605c9
Santhosh-testsigma <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-08-06 18:00:36 +0530

    trail and error 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -76,6 +76,12 @@ module.exports = {
               iframeId: false,
             },
           },
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 800, // as this doc repo does not require more than 900px size images.
+            },
+          },
         ],
       },
     },
