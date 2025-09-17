3d07b63a3ad76c900d9b7b1bebe6d8514164375f
Santhoshkumar <117272529+Santhosh-testsigma@users.noreply.github.com>
2025-09-17 18:43:18 +0530

    [WEB-1924] - Added Salesforce Chatbot on all child pages (#784)
    * [WEB-1924] - Added Salesforce Chatbot on all child pages
    
    * fix build issue

--- src/templates/page.jsx
+++ src/templates/page.jsx
@@ -9,6 +9,7 @@ import LeftNav from '../components/LeftNav';
 import SideBar from '../components/SideBar';
 import EditDoc from '../components/EditButton';
 import Footer from '../components/Footer';
+import SalesforceChatbot from '../components/third-party-integrations/sf-chatbot';
 
 import 'prismjs/themes/prism-tomorrow.css';
 import './page.scss';
@@ -204,6 +205,7 @@ export default ({ data, pageContext }) => {
           </div>
         </div>
       </div>
+      <SalesforceChatbot />
     </Layout>
   );
 };
